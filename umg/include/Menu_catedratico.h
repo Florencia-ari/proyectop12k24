b7126baaaec24df853404ee2745bfaa3f15cd0d4
vicomar <victorgomezcarrascosa@gmail.com>
2024-04-25 21:13:25 -0600

    subida de login y menus catedraticos

---
+++ umg/include/Menu_catedratico.h
@@ -0,0 +1,17 @@
+#ifndef MENU_CATEDRATICO_H
+#define MENU_CATEDRATICO_H
+
+
+class Menu_catedratico
+{
+    public:
+        Menu_catedratico();
+
+        void Menu_P();
+
+    protected:
+
+    private:
+};
+
+#endif // MENU_CATEDRATICO_H
---
+++ umg/src/Menu_catedratico.cpp
@@ -0,0 +1,73 @@
+#include "Menu_catedratico.h"
+#include "Usuario_Catedratico.h"
+#include<iostream>
+#include<fstream>
+#include<stdlib.h>
+#include<cstdlib>
+#include<conio.h>
+#include<iomanip>
+using namespace std;
+Menu_catedratico::Menu_catedratico()
+{
+}
+
+void Menu_catedratico::Menu_P()
+{
+
+ //variables de user y contraseñan
+    string user,contrasena;
+    //llmamos al objeto e ingresamos los parametros
+    Usuario_Catedratico ingreso(user,contrasena);
+
+
+    //creamos un bool  que verifique y despliegue el metodo verificarUsuario
+    bool UsuarioCorrecto=ingreso.VerificarUsuario();
+
+
+    //luego de ingresar con usuario y contraseña se nos desplega otro menu
+    if(UsuarioCorrecto)
+    {
+        int opcion;
+        char  x;
+        do
+        {
+        	system("cls");
+
+    cout <<"\t\t\t--------------------------------------------"<<endl;
+    cout <<"\t\t\t |      BIENVENIDO AL MENU CATEDRATICO      |"<<endl;
+    cout <<"\t\t\t--------------------------------------------"<<endl;
+	cout<<"\t\t\t 1. Asignacion De Cursos"<<endl;
+	cout<<"\t\t\t 2. Registros De Notas"<<endl;
+	cout<<"\t\t\t 3. Estado De Cuenta"<<endl;
+	cout<<"\t\t\t 4. Actas"<<endl;
+	cout<<"\t\t\t 5. Exit"<<endl;
+
+	cout<<"\t\t\t-------------------------------"<<endl;
+	cout<<"\t\t\tOpcion a escoger:[1/2/3/4/5]"<<endl;
+	cout<<"\t\t\t-------------------------------"<<endl;
+	cout<<"Ingresa tu Opcion: ";
+    cin>>opcion;
+    switch(opcion){
+    case 1:
+
+        break;
+    case 2:
+
+        break;
+    case 3:
+
+        break;
+    case 4:
+
+        break;
+    case 5:
+        break;
+	default:
+		cout<<"\n\t\t\t Opcion invalida...Por favor prueba otra vez..";
+        break;}
+        }while(opcion!= 5);
+
+    }
+
+}
+
